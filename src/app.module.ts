c2b5f17bd832813aafcf5a21071d9c5faa2bd096
Erfan Yousefi <erfanyousefi.co@gmail.com>
2024-08-21 10:20:16 +0330

    add project to github

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import {Module} from "@nestjs/common";
+// import {ChatGateway} from "./chat/chat.gateway";
+import {GroupGateway} from "./group/group.gateway";
+import {ChannelGateway} from "./channel/channel.gateway";
+import {IndexGateway} from "./index.gateway";
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [IndexGateway, GroupGateway, ChannelGateway],
+})
+export class AppModule {}
